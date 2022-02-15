37e942ce17ca52f71fe2b4bf4468fc2dcb1fe158
JeanOviedo <85713073+JeanOviedo@users.noreply.github.com>
2022-02-15 15:28:31 -0500

    ...

--- src/db.js
+++ src/db.js
@@ -8,6 +8,12 @@ const {
 } = process.env;
 
 const sequelize = new Sequelize(DATABASE_URL, {
+  dialectOptions: {
+    ssl : {
+      require : true,
+      rejectUnauthorized:false
+    },
+  }
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
