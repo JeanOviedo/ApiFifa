d53c0258eeea6a6a7e70df27659dc66f45d4debf
JeanOviedo <85713073+JeanOviedo@users.noreply.github.com>
2022-02-15 16:48:08 -0500

    update

--- src/db.js
+++ src/db.js
@@ -7,16 +7,35 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST, DATABASE_URL
 } = process.env;
 
-const sequelize = new Sequelize(DATABASE_URL, {
-  dialectOptions: {
-    ssl : {
-      require : true,
-      rejectUnauthorized:false
-    }
-  },
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-});
+
+
+let sequelize;
+
+
+if (DATABASE_URL) {
+  //crea el sequelize con la base de datos remota
+  sequelize = new Sequelize(DATABASE_URL, {
+    logging: false, // set to console.log to see the raw SQL queries
+    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+  });
+
+}
+
+else {
+  //crea el sequelize
+  sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`, {
+    logging: false, // set to console.log to see the raw SQL queries
+    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  });
+
+}
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
